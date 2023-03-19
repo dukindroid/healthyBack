bb611dae1d1f5ac68aba097559b2e9e8afaac067
Javier Gutiérrez <dukintosh@gmail.com>
2023-03-19 05:41:11 -0600

    cors: access control allow origin all

--- src/app.js
+++ src/app.js
@@ -17,14 +17,14 @@ server.use(cookieParser());
 server.use(
   morgan(":method :url :status :res[content-length] - :response-time ms")
 );
-// server.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Credentials', 'true')
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Credentials', 'true')
 
-//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
-//   next()
-// })
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
 server.use("/", routes);
 
